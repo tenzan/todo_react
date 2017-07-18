3373a642286d9c79c41b25dff3edc6d0f5248e64
Askar Karasaev <askar.karasaev@gmail.com>
2017-07-18 11:45:50 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos:  [
+        {id: 1, name:'Task 1', isComplete: true},
+        {id: 2, name:'Task 2', isComplete: false},
+        {id: 3, name:'Task 3', isComplete: false}
+      ],
+      currentTodo: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange(evt) {
+    this.setState({
+      currentTodo: evt.target.value
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Todos</h2>
+        </div>
+        <div className="Todo-App">
+          <form>
+             <input type="text" onChange={this.handleInputChange}
+               value={this.state.currentTodo}/>
+          </form>
+          <div className="Todo-List">
+            <ul>
+               {this.state.todos.map(todo =>
+                 <li key={todo.id}>
+                    <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
+                 </li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
