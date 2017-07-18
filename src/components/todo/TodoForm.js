4469ebde11e8bb03dcee479648419680e4a8050c
Askar Karasaev <askar.karasaev@gmail.com>
2017-07-18 12:55:44 +0900

    Validate Component Input with Prop Types

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const TodoForm = (props) => (
   <form>
      <input type="text"
        onChange={props.handleInputChange}
        value={props.currentTodo}/>
-  </form>
-)
+  </form>)
+
+  TodoForm.propTypes = {
+    currentTodo: PropTypes.string.isRequired,
+    handleInputChange: PropTypes.func.isRequired
+  }
